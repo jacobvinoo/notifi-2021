c73158b7632f4c74d4030049a19584d8e036357b
Vinoo Jacob <vinoojacob@Vinoos-Mac-mini.local>
2021-02-08 12:18:51 +1300

    Fixed CSS issue with main content formatting

--- source/config/settings.py
+++ source/config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,8 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+mimetypes.add_type("text/css", ".css", True)
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
